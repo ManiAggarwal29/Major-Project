404c081bbf8f7d7329c3d7a070c2f166f0969652
ManiAggarwal29 <maniaggarwal29@gmail.com>
2025-12-03 12:23:18 +0530

    fileops.h

--- include/fileops.h
+++ include/fileops.h
@@ -1,12 +1,8 @@
 // fileops.h - file related work (very simple)
 #ifndef FILEOPS_H
 #define FILEOPS_H
-
 #include "ticket.h"
-
 void addTicketToFile(const Ticket *t);  // saving ticket
 int  removeTicketByID(int id);          // deleting ticket
 void clearInput(void);                  // clear wrong input from stdin
-
 #endif
-
