9488121245eb7d5f2389f2997b3ad4725f22c5c0
ManiAggarwal29 <maniaggarwal29@gmail.com>
2025-12-03 12:22:33 +0530

    ticket.h

--- include/ticket.h
+++ include/ticket.h
@@ -1,10 +1,7 @@
 // ticket.h - header file for ticket related details
-
 #ifndef TICKET_H
 #define TICKET_H
-
 #define DATAFILE "tickets.txt"
-
 // Structure for storing one ticket
 typedef struct {
     int id;                 // ticket id
@@ -14,17 +11,14 @@ typedef struct {
     int seatNo;             // seat number
     int fare;               // ticket fare
 } Ticket;
-
 // Structure for bus details
 typedef struct {
     int busNo;              // bus number
     char destination[50];   // city name
     int fare;               // fixed fare for that bus
 } Bus;
-
 // fixed buses we provide (defined in ticket.c)
 extern Bus buses[4];
-
 // all functions
 int getNextTicketID(void);
 int seatTaken(int busNo, int seatNo);
@@ -33,6 +27,4 @@ void viewAllTickets(void);
 void viewBusTickets(void);
 void viewByDestination(void);
 void cancelTicket(void);
-
 #endif
-
