60ac58d01f811af7efb1e05af2cd6fed51c51df0
ManiAggarwal29 <maniaggarwal29@gmail.com>
2025-12-03 12:17:37 +0530

    Update fileops.c

--- src/fileops.c
+++ src/fileops.c
@@ -1,16 +1,11 @@
-
 // fileops.c - handles file reading/writing in TEXT mode
 #include <stdio.h>
 #include <stdlib.h>
 #include "fileops.h"
-
 // Format (one ticket per line) in tickets.txt:
-//
 // id busNo seatNo name destination fare
-//
 // Example:
 // 1 101 5 Rahul Delhi 500
-
 // append new ticket at end of text file
 void addTicketToFile(const Ticket *t) {
     FILE *fp = fopen(DATAFILE, "a");   // text append mode
@@ -18,7 +13,6 @@ void addTicketToFile(const Ticket *t) {
         perror("Error opening ticket file");
         return;
     }
-
     // NOTE: name and destination should NOT contain spaces
     fprintf(fp, "%d %d %d %s %s %d\n",
             t->id,
@@ -27,10 +21,8 @@ void addTicketToFile(const Ticket *t) {
             t->name,
             t->destination,
             t->fare);
-
     fclose(fp);
 }
-
 // remove ticket using a temporary text file
 int removeTicketByID(int id) {
     FILE *fp = fopen(DATAFILE, "r");
@@ -38,17 +30,14 @@ int removeTicketByID(int id) {
         // file does not exist -> nothing to delete
         return 0;
     }
-
     FILE *temp = fopen("temp.txt", "w");
     if (!temp) {
         perror("Error opening temp file");
         fclose(fp);
         return 0;
     }
-
     Ticket t;
     int found = 0;
-
     // read each line in text format
     while (fscanf(fp, "%d %d %d %49s %49s %d",
                   &t.id,
@@ -57,7 +46,6 @@ int removeTicketByID(int id) {
                   t.name,
                   t.destination,
                   &t.fare) == 6) {
-
         if (t.id == id) {
             found = 1;      // skip this ticket
         } else {
@@ -71,20 +59,16 @@ int removeTicketByID(int id) {
                     t.fare);
         }
     }
-
     fclose(fp);
     fclose(temp);
-
     if (found) {
         remove(DATAFILE);
         rename("temp.txt", DATAFILE);
     } else {
         remove("temp.txt");
     }
-
     return found;
 }
-
 // clears remaining characters from stdin when wrong input is given
 void clearInput(void) {
     int c;
@@ -92,4 +76,3 @@ void clearInput(void) {
         // discard
     }
 }
-
